94ea34751ca32950b3b87afc4632933f575d88a3
AntonRabyshev <102146208+AntonRabyshev@users.noreply.github.com>
2022-06-13 20:10:37 +0300

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -5,7 +5,7 @@ python ./sender.py &
 # Start the second process
 python ./parser.py &
 # Start the third process
-python ./error_handler.py &
+python ./error_handler.py 
 # Start the fourth process
 # следующий процесс выполняется in-place, поэтому не запускаем
-#python ./file_generator.py &
+#python ./file_generator.py 
